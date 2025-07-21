142e6f6800b4730115fcc047b32c330e8fd26516
Mahfuz <aamahfuz.pro@gmail.com>
2025-07-22 01:27:04 +0600

    api: modifed api

--- src/api/eventApi.ts
+++ src/api/eventApi.ts
@@ -1,24 +1,27 @@
 import axios from "axios";
-import type { Event } from "../types/event";
+import type { Event, NewEventInput } from "../types/event";
 
-const API_URL = "http://localhost:5000/api/events";
+const API_URL = import.meta.env.VITE_API_URL + "/events";
 
-export const getEvents = async () => {
-  const response = await axios.get(API_URL);
-  return response.data;
+export const getEvents = async (): Promise<Event[]> => {
+  const res = await axios.get(API_URL);
+  return res.data;
 };
 
-export const createEvent = async (event: Omit<Event, "_id">) => {
-  const response = await axios.post(API_URL, event);
-  return response.data;
+export const createEvent = async (event: NewEventInput): Promise<Event> => {
+  const res = await axios.post(API_URL, event);
+  return res.data;
 };
 
-export const updateEvent = async (id: string, event: Partial<Event>) => {
-  const response = await axios.put(`${API_URL}/${id}`, event);
-  return response.data;
+export const updateEvent = async (
+  id: string,
+  event: Partial<Event>
+): Promise<Event> => {
+  const res = await axios.put(`${API_URL}/${id}`, event);
+  return res.data;
 };
 
 export const deleteEvent = async (id: string) => {
-  const response = await axios.delete(`${API_URL}/${id}`);
-  return response.data;
+  const res = await axios.delete(`${API_URL}/${id}`);
+  return res.data;
 };
